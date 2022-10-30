62dfea5236b285f5c72d425daa2b8f95db90717b
Larissa <larissa@fantasticocomic.com>
2022-10-30 16:42:05 -0600

    Update header, finderbox and global styles

---
+++ src/components/FinderBox.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="finder">
+    <input
+      class="finder__input"
+      :name="inputName"
+      :placeholder="placeholderInput"
+      ref="input"
+      :value="value"
+      @input="updateValue()"
+      @keyup="keyup()"
+      @keyup.enter="keyupEnter()"
+      @blur="blur()"
+    />
+    <span class="finder__icon" @click="iconClick"></span>
+  </div>
+</template>
+<script>
+export default {
+  name: "FinderBox",
+
+  props: {
+    inputName: {
+      type: String,
+    },
+    placeholderInput: {
+      type: String,
+    },
+    value: {
+      type: String,
+    },
+  },
+
+  mounted() {
+    if (this.autofocus) this.$refs.input.focus();
+  },
+
+  methods: {
+    updateValue() {
+      this.$emit("input", this.$refs.input.value);
+    },
+    keyup() {
+      this.$emit("keyup");
+    },
+    keyupEnter() {
+      this.$emit("keyupEnter");
+    },
+    blur() {
+      this.$emit("blur");
+    },
+    iconClick() {
+      this.$emit("iconClick");
+    },
+  },
+};
+</script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,21 @@
 <template>
   <header class="header">
-    <div class="header__logo"></div>
+    <div class="header__container container__inner">
+      <div class="header__logo"></div>
+      <FinderBox placeholderInput="Buscar productos, marcas y más..." />
+      <router-link class="header__promo" to="/">
+        <img src="../assets/promo_disney.webp" alt="FREETRIAL" />
+      </router-link>
+    </div>
   </header>
 </template>
 <script>
+import FinderBox from "@/components/FinderBox.vue";
 export default {
   name: "HeaderUI",
+
+  components: {
+    FinderBox,
+  },
 };
 </script>
