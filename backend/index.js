f022280b220cb94cd2de9196da2fdaffc83f7ee2
Larissa <larissa@fantasticocomic.com>
2022-10-31 18:00:00 -0600

    Set initial be configuration

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const { urlencoded } = require("express");
+const app = express();
+
+const allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Accept,"
+  );
+  next();
+};
+
+app.use(express.json());
+app.use(urlencoded({ extended: false }));
+app.options("*", cors());
+
+app.set("port", process.env.PORT || 4000);
+
+app.listen(app.get("port"));
+console.log(
+  "Server is ready on port",
+  app.get("port"),
+  "MeLi Server is running!"
+);
