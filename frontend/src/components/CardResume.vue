c8724c581cd7241cbed17132786b95967d0b10e8
Larissa <larissa@fantasticocomic.com>
2022-11-02 18:54:27 -0600

    Add mocks and props in CardResume

--- frontend/src/components/CardResume.vue
+++ frontend/src/components/CardResume.vue
@@ -2,24 +2,39 @@
   <div class="card__wrapper">
     <div class="card">
       <figure class="card__image">
-        <img
-          src="https://http2.mlstatic.com/D_Q_NP_2X_629197-MLM45309720387_032021-AB.webp"
-          alt="alt description"
-        />
+        <img :src="imageSource" alt="alt description" />
       </figure>
       <div class="card__info">
-        <div class="card__price">$1.980</div>
+        <div class="card__price">${{ price }}</div>
         <div class="card__description">
-          Vasito Karla Sabor Tamarindo Y Guayaba Dulce 24 Piezas
+          {{ description }}
         </div>
       </div>
-      <div class="card__location">Capital Federal</div>
+      <div class="card__location">{{ location }}</div>
     </div>
   </div>
 </template>
 <script>
 export default {
   name: "CardResume",
-  props: {},
+  props: {
+    imageSource: {
+      type: String,
+      default:
+        "https://http2.mlstatic.com/D_Q_NP_2X_629197-MLM45309720387_032021-AB.webp",
+    },
+    price: {
+      type: String,
+      default: "1.980",
+    },
+    description: {
+      type: String,
+      default: "Vasito Karla Sabor Tamarindo Y Guayaba Dulce 24 Piezas",
+    },
+    location: {
+      type: String,
+      default: "Capital Federal",
+    },
+  },
 };
 </script>
